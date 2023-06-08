9d37b3002f11c46e9eed442cde9043fa925cb490
Chez <lucaschezin@gmail.com>
2023-06-08 20:54:02 -0300

    hello guys

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,53 @@
-"use client"
-import React, { useEffect, useRef } from 'react';
+"use client";
+import React, { useEffect, useRef } from "react";
 
 const IndexPage = () => {
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <div className="invert filter filter:hue-rotate(180deg)">
-        <img src="/spiral.gif" rel="multiple circular spirals" alt="lucas contini" className="w-40 h-40 mb-8"/>
+        <img
+          src="/spiral.gif"
+          rel="multiple circular spirals"
+          alt="lucas contini"
+          className="w-40 h-40 mb-8"
+        />
       </div>
       <ul className="flex space-x-5">
         <li>
-          <img src="/soundcloud.svg" rel="soundcloud" href="https://soundcloud.com/lucas-contini" className="hover:scale-110 duration-00" />
+          <a href="https://soundcloud.com/lucas-contini">
+            <img
+              src="/soundcloud.svg"
+              rel="soundcloud"
+              className="hover:scale-110 duration-00"
+            />
+          </a>
         </li>
         <li>
-          <img src="/instagram.svg" rel="instagram" href="https://instagram.com/lucasccontini" className="hover:scale-110 duration-00" />
+          <a href="https://instagram.com/lucasccontini">
+            <img
+              src="/instagram.svg"
+              rel="instagram"
+              className="hover:scale-110 duration-00"
+            />
+          </a>
         </li>
         <li>
-          <img src="/youtube.svg" rel="youtube" href="https://youtube.com/ch3zin" className="hover:scale-110 duration-00" />
+          <a href="https://youtube.com/ch3zin">
+            <img
+              src="/youtube.svg"
+              rel="youtube"
+              className="hover:scale-110 duration-00"
+            />
+          </a>
         </li>
         <li>
-          <img src="spotify.svg" rel="spotify" href="https://open.spotify.com/user/lucaschezin" className="hover:scale-110 duration-00" />
+          <a href="https://open.spotify.com/user/lucaschezin">
+            <img
+              src="/spotify.svg"
+              rel="spotify"
+              className="hover:scale-110 duration-00"
+            />
+          </a>
         </li>
       </ul>
     </div>
